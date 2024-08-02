93ca8c2587a9bc13fff218d9e4639c017f2aa89b
9ziggy9 <davidarogers@protonmail.com>
2024-08-01 20:42:57 -0700

    successfully probing host

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "../style.css"
 
+const HOST_URL: string = "https://9ziggy9.share.zrok.io"
 const LOAD_MSG_ELEMS: string[] = [
 `
   <div class="center-field" id="state-connect">
@@ -21,7 +22,7 @@ const LOAD_MSG_ELEMS: string[] = [
 `,
 `
   <div class="center-field" id="state-connect">
-    <p>success</p>
+      <p>success, redirecting ...</p>
       <span id="loading" class="check-icon material-symbols-outlined">
         check
       </span>
@@ -39,6 +40,15 @@ const sleep = (ms: number): Promise<void> => {
   return new Promise((res) => { setTimeout(() => res(), ms); });
 }
 
+async function isHostAvailable(url: string): Promise<boolean> {
+  try {
+    const response = await fetch(url, {method: "HEAD", mode: "cors"});
+    return response.ok;
+  } catch (_) {
+    return false;
+  }
+}
+
 async function main(): Promise<void> {
   const divLoadingArea = document.getElementById("load-area");
   if (divLoadingArea) {
@@ -46,7 +56,15 @@ async function main(): Promise<void> {
     await sleep(3000);
     divLoadingArea.innerHTML = LOAD_MSG_ELEMS[1];
     await sleep(4000);
-    divLoadingArea.innerHTML = LOAD_MSG_ELEMS[3];
+
+    if (await isHostAvailable(HOST_URL)) {
+      divLoadingArea.innerHTML = LOAD_MSG_ELEMS[2];
+      await sleep(1500);
+      window.location.replace(HOST_URL);
+    } else {
+      divLoadingArea.innerHTML = LOAD_MSG_ELEMS[3];
+    }
+
   } 
 }
 
